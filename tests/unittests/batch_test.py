e2c77c8c39ee18ebd6bbfd9267e4421479b042f6
Hajime Kawahara <divrot@gmail.com>
2024-10-14 08:38:56 +0900

    unit test for batch

---
+++ tests/unittests/batch_test.py
@@ -0,0 +1,31 @@
+from exoemulator.model.batch import generate_batches
+
+import jax.numpy as jnp
+
+
+def test_generate_batches():
+    x = jnp.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y = jnp.array([1, 2, 3, 4])
+    batch_size = 2
+
+    batches = list(generate_batches(x, y, batch_size))
+
+    assert len(batches) == 2
+    assert (batches[0][0] == jnp.array([[1, 2], [3, 4]])).all()
+    assert (batches[0][1] == jnp.array([1, 2])).all()
+    assert (batches[1][0] == jnp.array([[5, 6], [7, 8]])).all()
+    assert (batches[1][1] == jnp.array([3, 4])).all()
+
+
+def test_generate_batches_incomplete_batch():
+    x = jnp.array([[1, 2], [3, 4], [5, 6]])
+    y = jnp.array([1, 2, 3])
+    batch_size = 2
+
+    batches = list(generate_batches(x, y, batch_size))
+
+    assert len(batches) == 2
+    assert (batches[0][0] == jnp.array([[1, 2], [3, 4]])).all()
+    assert (batches[0][1] == jnp.array([1, 2])).all()
+    assert (batches[1][0] == jnp.array([[5, 6]])).all()
+    assert (batches[1][1] == jnp.array([3])).all()
