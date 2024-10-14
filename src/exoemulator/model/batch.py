298f24909fe106ff87dc450664fd13863d598c34
Hajime Kawahara <divrot@gmail.com>
2024-10-14 09:08:10 +0900

    output is list of device array

--- src/exoemulator/model/batch.py
+++ src/exoemulator/model/batch.py
@@ -1,15 +1,21 @@
-def generate_batches(x, y, batch_size):
-    """generate batches
+import jax.numpy as jnp
+
+
+def generate_batches(x, batch_size: int):
+    """generate batches as a list of device arrays
 
     Args:
-        x (jnp.array): x
-        y (jnp.array): y
+        x (np.array): x
         batch_size (int): batch size
 
     Returns:
-        generator
+        list of device arrays (jnp.array)
+
     """
     n = x.shape[0]
-    for i in range(0, n, batch_size):
-        yield x[i:i + batch_size], y[i:i + batch_size]
+    device_arrays = []
 
+    for i in range(0, n, batch_size):
+        device_arrays.append(jnp.asarray(x[i : i + batch_size]))
+
+    return device_arrays
--- tests/unittests/batch_test.py
+++ tests/unittests/batch_test.py
@@ -3,40 +3,18 @@ from exoemulator.model.batch import generate_batches
 import jax.numpy as jnp
 
 
-def test_generate_batches():
-    x = jnp.array([[1, 2], [3, 4], [5, 6], [7, 8]])
-    y = jnp.array([1, 2, 3, 4])
-    batch_size = 2
+def test_generate_batches_single():
+    import numpy as np
 
-    batches = list(generate_batches(x, y, batch_size))
-
-    assert len(batches) == 2
-    assert (batches[0][0] == jnp.array([[1, 2], [3, 4]])).all()
-    assert (batches[0][1] == jnp.array([1, 2])).all()
-    assert (batches[1][0] == jnp.array([[5, 6], [7, 8]])).all()
-    assert (batches[1][1] == jnp.array([3, 4])).all()
-
-
-def test_generate_batches_incomplete_batch():
-    x = jnp.array([[1, 2], [3, 4], [5, 6]])
-    y = jnp.array([1, 2, 3])
-    batch_size = 2
-
-    batches = list(generate_batches(x, y, batch_size))
-
-    assert len(batches) == 2
-    assert (batches[0][0] == jnp.array([[1, 2], [3, 4]])).all()
-    assert (batches[0][1] == jnp.array([1, 2])).all()
-    assert (batches[1][0] == jnp.array([[5, 6]])).all()
-    assert (batches[1][1] == jnp.array([3])).all()
-
-def test_generate_batches_temp():
-    data = jnp.array([1,2,3,4,5,6,7,8,9,10])
+    data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     batch_size = 4
-    batches = list(generate_batches(data, data, batch_size))
+    batches = generate_batches(data, batch_size)
     print(batches)
 
+    assert (batches[0] == jnp.array([1, 2, 3, 4])).all()
+    assert (batches[1] == jnp.array([5, 6, 7, 8])).all()
+    assert (batches[2] == jnp.array([9, 10])).all()
+
+
 if __name__ == "__main__":
-    test_generate_batches()
-    test_generate_batches_incomplete_batch()
-    test_generate_batches_temp()
+    test_generate_batches_single()
