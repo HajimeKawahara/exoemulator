db2c74fe5ca6fda3c0f34f2ecce3627072c096f8
Hajime Kawahara <divrot@gmail.com>
2024-10-23 08:39:48 +0900

    sampling in opacity

--- src/exoemulator/train/opacity.py
+++ src/exoemulator/train/opacity.py
@@ -5,7 +5,8 @@
 """
 
 from exoemulator.utils.checkpackage import check_installed
-
+from exoemulator.train.sampling import latin_hypercube_sampling
+import numpy as np
 
 class OptExoJAX:
     """Opacity Training with ExoJAX"""
@@ -21,8 +22,15 @@ class OptExoJAX:
             print("opa in opt: ", opa.__class__.__name__)
             self.opa = opa
 
-    def generate_mini_batches():
-        pass
+    def generate_batch(self, trange, prange, nsample, method="lhs"):
+        if method == "lhs":
+            # tarr is linear, parr is log
+            samples = latin_hypercube_sampling(trange, np.log10(prange), nsample)
+            tarr = samples[:, 0]
+            parr = 10**(samples[:, 1])
+        else:
+            raise NotImplementedError(f"Method {method} is not implemented")
+        return tarr, parr, self.opa.xsmatrix(tarr, parr)
 
 
 if __name__ == "__main__":
--- tests/unittests/train/opacity_test.py
+++ tests/unittests/train/opacity_test.py
@@ -1,5 +1,6 @@
 from exoemulator.utils.checkpackage import check_installed
 from exoemulator.train.opacity import OptExoJAX
+import numpy as np
 
 
 def test_OptExoJAX():
@@ -18,12 +19,20 @@ def test_OptExoJAX_opa_premodit():
         mdb = mock_mdbExomol()
         opa = OpaPremodit(mdb, nu_grid, auto_trange=[490.0, 510.0])
         opt = OptExoJAX(opa=opa)
+        nsample = 10
+        tarr, parr, xs = opt.generate_batch(
+            trange=[490.0, 510.0], prange=[1.0e-3, 1.0e3], nsample=nsample, method="lhs"
+        )
+        tarr2, parr2, xs2 = opt.generate_batch(
+            trange=[490.0, 510.0], prange=[1.0e-3, 1.0e3], nsample=nsample, method="lhs"
+        )
 
-        assert opt.opa.method == "premodit"
+        assert np.all(tarr != tarr2) and tarr.shape == (nsample,)
+        assert np.all(parr != parr2) and parr.shape == (nsample,)
+        assert np.all(xs != xs2) and xs.shape == (nsample, len(nu_grid))
+
     else:
         pass
-
-
 
 
 if __name__ == "__main__":
