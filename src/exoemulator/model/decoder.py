cc0f63f903e16fadc98a9144873507e9aac30288
Hajime Kawahara <divrot@gmail.com>
2024-10-31 09:51:49 +0900

    loss and test loss

--- src/exoemulator/model/decoder.py
+++ src/exoemulator/model/decoder.py
@@ -16,15 +16,12 @@ class EmuMlpDecoder(nnx.Module):
     """
 
     def __init__(self, *, rngs: nnx.Rngs, grid_length: int):
-        #self.Nneuron_last=1024
-        self.Nneuron_last = 512
         self.dense_entrance = nnx.Linear(in_features=2, out_features=16, rngs=rngs)
         self.dense_1 = nnx.Linear(in_features=16, out_features=32, rngs=rngs)
         self.dense_2 = nnx.Linear(in_features=32, out_features=256, rngs=rngs)
-        self.dense_3 = nnx.Linear(in_features=256, out_features=self.Nneuron_last, rngs=rngs)
-
+        self.dense_3 = nnx.Linear(in_features=256, out_features=512, rngs=rngs)
         self.dense_out = nnx.Linear(
-            in_features=self.Nneuron_last, out_features=grid_length, rngs=rngs
+            in_features=512, out_features=grid_length, rngs=rngs
         )
 
     def __call__(self, input_parameter):
@@ -33,40 +30,4 @@ class EmuMlpDecoder(nnx.Module):
         x = nnx.gelu(self.dense_2(x))
         x = nnx.gelu(self.dense_3(x))
         return self.dense_out(x)
-        # return nnx.sigmoid(self.dense_out(x)) #limit to [0,1]
 
-
-class EmuUpsDecoder(nnx.Module):
-    """simple upsampling decoder type neuralnet emulator model (not working yet)
-
-    Args:
-        nnx (_type_): nnx module
-
-    """
-
-    # assuming grid_length = 20000
-    def __init__(self, *, rngs: nnx.Rngs, grid_length: int):
-        self.dense_entrance = nnx.Linear(in_features=2, out_features=16, rngs=rngs)
-        self.dense_1 = nnx.Linear(in_features=16, out_features=32, rngs=rngs)
-        self.dense_2 = nnx.Linear(in_features=32, out_features=256, rngs=rngs)
-        self.dense_3 = nnx.Linear(in_features=256, out_features=1000, rngs=rngs)
-
-        # input should have the length = 5000
-        exansion = 20
-        self.transconv_out = nnx.ConvTranspose(
-            in_features=1,
-            out_features=1,
-            strides=exansion,
-            kernel_size=(1,),
-            padding="SAME",
-            rngs=rngs,
-        )
-
-    def __call__(self, input_parameter):
-        x = nnx.gelu(self.dense_entrance(input_parameter))
-        x = nnx.gelu(self.dense_1(x))
-        x = nnx.gelu(self.dense_2(x))
-        x = nnx.gelu(self.dense_3(x))
-        x = self.transconv_out(x[:, :, jnp.newaxis])
-        return x[:, :, 0]
-        # return nnx.sigmoid(self.dense_out(x)) #limit to [0,1]
--- tests/integration/train/train_test.py
+++ tests/integration/train/train_test.py
@@ -1,7 +1,7 @@
+import test
 from exoemulator.train.opacity import OptExoJAX
 from exoemulator.model.mlp import EmuMlp
 from exoemulator.model.decoder import EmuMlpDecoder
-from exoemulator.model.decoder import EmuUpsDecoder
 
 from flax import nnx
 import numpy as np
@@ -11,7 +11,9 @@ import tqdm
 import optax
 
 import orbax.checkpoint as ocp
-ckpt_dir = ocp.test_utils.erase_and_create_empty('/home/kawahara/checkpoints')
+
+ckpt_dir = ocp.test_utils.erase_and_create_empty("/home/kawahara/checkpoints")
+
 
 def test_training():
     from exojax.test.emulate_mdb import mock_mdbExomol
@@ -30,37 +32,27 @@ def test_training():
     opa = OpaPremodit(mdb, nu_grid, auto_trange=trange)
     # emulator_model = EmuMlp(rngs=nnx.Rngs(0), grid_length=len(nu_grid))
     emulator_model = EmuMlpDecoder(rngs=nnx.Rngs(0), grid_length=len(nu_grid))
-    # emulator_model = EmuUpsDecoder(rngs=nnx.Rngs(0), grid_length=len(nu_grid)) #not working
     opt = OptExoJAX(opa=opa)
 
     # optimizer
-    # learning_rate = 1e-3
     learning_rate = 1e-4
     lrlog = int(np.log10(learning_rate))
-
     momentum = 0.9
     optimizer = nnx.Optimizer(emulator_model, optax.adamw(learning_rate, momentum))
 
     # defines metrics
     metrics = nnx.MultiMetric(loss=nnx.metrics.Average("loss"))
     nsample = 50  # default 300
-    niter = 1000000
-    outfile = (
-        "mlp_emulator_R"
-        + str(lrlog)
-        + "_L"
-        + str(emulator_model.Nneuron_last)
-        + "n"
-        + str(nsample)
-        + "niter"
-        + str(niter)
-        + ".png"
-    )
+    niter = 3000000
+    #niter = 10000
+    tag = str(lrlog) + "n" + str(nsample) + "niter" + str(niter)
+    outfile = "mlp_emulator_R" + tag + ".png"
     print("outfile:", outfile)
 
-    # for i in tqdm.tqdm(range(10000)):
+    lossarr = []
+    testlossarr = []
+
     for i in tqdm.tqdm(range(niter)):
-        # for i in tqdm.tqdm(range(1000)):
         input_parameters, logxs = opt.generate_batch(
             trange=trange, prange=prange, nsample=nsample, method="lhs"
         )
@@ -72,32 +64,54 @@ def test_training():
                 trange=trange, prange=prange, nsample=nsample, method="lhs"
             )
             testloss = opt.evaluate_step(emulator_model, input_parameters, logxs)
-            print(loss, testloss)
+            lossarr.append(loss)
+            testlossarr.append(testloss)
+            
+    # plot loss
+    np.savez("loss"+tag+".npz", lossarr=lossarr, testlossarr=testlossarr)
+    plt.plot(lossarr[10:], label="train")
+    plt.plot(testlossarr[10:], label="test")
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.legend()
+    plt.savefig("loss"+tag+".png")
 
+    # test prediction
     input_par = jnp.array([729.0, jnp.log10(3.0e-1)])
-    if isinstance(emulator_model, EmuUpsDecoder):
-        input_par_m = jnp.array([input_par])
-        output_vector = emulator_model(input_par_m)[0, :]
-    else:
-        output_vector = emulator_model(input_par)
+    output_vector = emulator_model(input_par)
     xs_ref = opt.opa.xsvector(input_par[0], 10 ** input_par[1])
-
     offset = 22.0
     factor = 0.3
+    xs_pred = xs_prediction(output_vector, offset, factor)
+
+    # plot
     fig = plt.figure()
     ax = fig.add_subplot(211)
-    plt.plot(nu_grid, np.log10(xs_ref), lw=1)
-    plt.plot(nu_grid, output_vector / factor - offset, alpha=0.2, lw=2)
+    plt.plot(nu_grid, xs_ref, lw=1)
+    plt.plot(nu_grid, xs_pred, alpha=0.2, lw=2)
+    plt.yscale("log")
+    ax.set_ylabel("cross section (cm2)")
     ax2 = fig.add_subplot(212)
-    plt.plot(nu_grid, np.log10(xs_ref) - (output_vector / factor - offset), lw=1)
+    plt.plot(nu_grid, 1.0 - xs_pred / xs_ref, lw=1)
     ax2.set_xlabel("wavenumber (cm-1)")
+    ax2.set_ylabel("relative error")
     plt.savefig(outfile)  # R: lerning rate 1e-4
     plt.show()
 
     _, state = nnx.split(emulator_model)
     nnx.display(state)
     checkpointer = ocp.StandardCheckpointer()
-    checkpointer.save(ckpt_dir / 'state', state)
+    checkpointer.save(ckpt_dir / "state", state)
+
+    # need to wait for the file to be written
+    from exoemulator.utils.sleep import wait_for_saving
+
+    wait_for_saving(waitsec=3)
+
+
+def xs_prediction(output_vector, offset, factor):
+    return 10 ** (output_vector / factor - offset)
+
 
 if __name__ == "__main__":
     test_training()
